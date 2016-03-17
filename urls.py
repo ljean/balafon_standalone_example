8a7407f98b90b1a08481eb8ecfe9576cb5fda12a
Luc Jean <ljean@apidev.fr>
2016-03-17 16:23:44 +0100

    Initial commit

---
+++ urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from balafon.urls import urlpatterns
+
+balafon_urlpatterns = urlpatterns
+
+urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns += balafon_urlpatterns
